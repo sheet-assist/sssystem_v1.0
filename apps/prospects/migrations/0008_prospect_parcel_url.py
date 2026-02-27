1dc97210d8b1772e9e3457cf1b73728dbc34741f
Bhanu Consulting <biome.consulting@outlook.com>
2026-02-27 22:51:27 +0530

    migration fixed

--- apps/prospects/migrations/0008_prospect_parcel_url.py
+++ apps/prospects/migrations/0008_prospect_parcel_url.py
@@ -1,26 +1,26 @@
 from django.db import migrations, models
 
 
-def remove_foreign_key_orphans(apps, schema_editor):
-    """
-    Remove any rows currently violating SQLite foreign keys so schema edits can run.
-    """
-    connection = schema_editor.connection
-    quote_name = connection.ops.quote_name
+# def remove_foreign_key_orphans(apps, schema_editor):
+#     """
+#     Remove any rows currently violating SQLite foreign keys so schema edits can run.
+#     """
+#     connection = schema_editor.connection
+#     quote_name = connection.ops.quote_name
 
-    with connection.cursor() as cursor:
-        while True:
-            cursor.execute("PRAGMA foreign_key_check")
-            violations = cursor.fetchall()
-            if not violations:
-                break
+#     with connection.cursor() as cursor:
+#         while True:
+#             cursor.execute("PRAGMA foreign_key_check")
+#             violations = cursor.fetchall()
+#             if not violations:
+#                 break
 
-            # PRAGMA result: (table, rowid, parent_table, fk_index)
-            for table_name, row_id, _parent, _fk_index in violations:
-                cursor.execute(
-                    f"DELETE FROM {quote_name(table_name)} WHERE rowid = %s",
-                    [row_id],
-                )
+#             # PRAGMA result: (table, rowid, parent_table, fk_index)
+#             for table_name, row_id, _parent, _fk_index in violations:
+#                 cursor.execute(
+#                     f"DELETE FROM {quote_name(table_name)} WHERE rowid = %s",
+#                     [row_id],
+#                 )
 
 
 class Migration(migrations.Migration):
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(remove_foreign_key_orphans, migrations.RunPython.noop),
+        # migrations.RunPython(remove_foreign_key_orphans, migrations.RunPython.noop),
         migrations.AddField(
             model_name="prospect",
             name="parcel_url",
